cbbf0db137785556b422ea25589ff1862cc45fe7
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-09-07 16:56:51 -0300

    feat: add next.js configuration files

---
+++ tailwind.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(20px)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 3s ease-out',
+        slideIn: 'slideIn 3s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
