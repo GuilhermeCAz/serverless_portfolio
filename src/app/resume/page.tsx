b3751bb6e06f553237f2cb40ee5545c381bf056d
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-10-03 13:00:24 -0300

    feat: add embedded resume page

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,12 @@
+export default function ResumePage() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <embed
+        src="/docs/Guilherme Azevedo - CV.pdf"
+        width="100%"
+        height="100%"
+        title="Guilherme Azevedo - Resume"
+      />
+    </div>
+  );
+}
