9f9e6f8fb5a1e8b50e1bf91c2d94b5cc9f5608e4
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-10-12 01:10:14 -0300

    feat: improve 404 page info

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
 export default function NotFound() {
   return (
-    <main className="flex flex-grow flex-col justify-center">
-      <h1 className="text-center text-6xl font-bold">404 | Not Found</h1>
-    </main>
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <h1 className="text-nowrap pb-4 text-center text-5xl font-bold lg:text-6xl 2xl:text-8xl">
+        404 | Page not found
+      </h1>
+
+      <p className="text-xl 2xl:text-2xl">
+        Sorry, the page you are looking for could not be found.
+      </p>
+
+      <p className="text-xl 2xl:text-2xl">
+        Please check your URL and try again.
+      </p>
+
+      <p className="pt-4 text-3xl font-bold hover:text-gray-500 hover:underline 2xl:text-4xl">
+        <Link href="/">Go back to home</Link>
+      </p>
+    </div>
   );
 }
