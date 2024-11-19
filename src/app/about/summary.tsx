ac5e3041fcd56c21d020a3fdf6d76301b39e0aa2
GuilhermeCAz <102599422+GuilhermeCAz@users.noreply.github.com>
2024-11-19 18:04:22 -0300

    feat(summary): improve summary text

--- src/app/about/summary.tsx
+++ src/app/about/summary.tsx
@@ -3,27 +3,34 @@ export default function Summary() {
   return (
     <section id="about" className="min-h-full snap-start">
       <Link href="#summary">
-        <h1 className="mb-8 pt-4 text-left text-6xl font-bold hover:text-gray-500 2xl:text-7xl">
+        <h1 className="mb-8 pt-4 text-left text-6xl font-bold hover:text-gray-500 2xl:pt-16 2xl:text-7xl">
           Resumo
         </h1>
       </Link>
 
-      <div className="text-justify text-2xl leading-relaxed 2xl:text-3xl">
-        <p className="mb-4">
-          Engenheiro de Backend com sólida experiência no desenvolvimento e
-          otimização de soluções orientadas a dados no setor financeiro.
-          Graduado em Engenharia de Produção pela UERJ e com vários anos de
-          experiência prática na Quantum, aprimorei minhas habilidades em
-          desenvolvimento full-stack (com foco em back-end), análise de dados,
-          automação e coordenação de equipes. Minha expertise abrange uma
-          variedade de tecnologias, frameworks e metodologias essenciais para o
-          desenvolvimento backend e engenharia de dados.
+      <div className="animate-fadeIn text-justify text-2xl leading-relaxed text-gray-300 2xl:text-3xl">
+        <p className="mb-4 md:text-left">
+          Sou um Engenheiro Full-Stack com experiência de 4+ anos no
+          desenvolvimento de software na{' '}
+          <Link
+            href="https://quantumfinance.com.br/"
+            target="_blank"
+            className="font-semibold text-gray-100 hover:text-gray-500"
+          >
+            Quantum
+          </Link>
+          , com enfoque em soluções orientadas a dados do mercado financeiro.
+          Minha expertise abrange uma variedade de tecnologias, frameworks e
+          metodologias essenciais para o desenvolvimento backend e engenharia de
+          dados.
         </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, facere
-          voluptates qui, rem numquam aliquid maiores dolor sed doloremque
-          necessitatibus aspernatur repellat voluptatem harum voluptatibus
-          dolorum, nesciunt expedita doloribus omnis.
+        <p className="md:text-left">
+          Tenho trabalhado constantemente para aprimorar minhas competências por
+          meio de projetos próprios e cursos especializados em Desenvolvimento
+          Web e DevOps. Estou motivado para colaborar em projetos que me
+          desafiem e proporcionem um ambiente de crescimento contínuo. Gosto de
+          ambientes de trabalho dinâmicos e colaborativos, onde a troca de
+          ideias e a inovação fazem parte do dia a dia.
         </p>
       </div>
     </section>
